282d0f47534357507fd03c0f8eec17b69245c561
Yanni Wang <yanni.wang@shopee.com>
2019-03-22 16:29:06 +0800

    no message

--- YNDetectWebBlankDemo/WKWebView/Cases/YNDemoWKWebComplexViewController.m
+++ YNDetectWebBlankDemo/WKWebView/Cases/YNDemoWKWebComplexViewController.m
@@ -17,6 +17,16 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self loadWithURLString:@"https://www.google.com/search?q=%E7%BE%8E%E5%A5%B3&newwindow=1&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjHz5PZipPhAhVjk-AKHSxrA68Q_AUIDigB&biw=1680&bih=916&dpr=2"];
+    
+    UIBarButtonItem *item = [[UIBarButtonItem alloc] initWithTitle:@"Next page" style:UIBarButtonItemStylePlain target:self action:@selector(didTapRightItem:)];
+    self.navigationItem.rightBarButtonItem = item;
+}
+
+- (void)didTapRightItem:(id)sender
+{
+    UIViewController *vc = [[UIViewController alloc] init];
+    vc.view.backgroundColor = [UIColor orangeColor];
+    [self.navigationController pushViewController:vc animated:YES];
 }
 
 @end
