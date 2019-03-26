dc15fd531c7312a4bad6740430f0794133c5d000
Yanni Wang <yanni.wang@shopee.com>
2019-03-26 18:50:25 +0800

    1

--- YNDetectWebBlank/Classes/UIView+YNDWB.m
+++ YNDetectWebBlank/Classes/UIView+YNDWB.m
@@ -97,7 +97,29 @@
 
 - (BOOL)yndwb_hookLoadingForUIIfNeed:(NSError**)error
 {
-    return NO;
+    // TODO not finish
+    __weak typeof(self) wself = self;
+    [self.KVOControllerNonRetaining observe:self keyPath:@"delegate" options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld block:^(id  _Nullable observer, id  _Nonnull object, NSDictionary<NSString *,id> * _Nonnull change) {
+        __strong typeof(self) self = wself;
+        id oldDelegate = change[NSKeyValueChangeOldKey];
+        if (oldDelegate) {
+            id<AspectToken> token = objc_getAssociatedObject(oldDelegate, @selector(yndwb_hookLoadingForUIIfNeed:));
+            if (token) {
+                BOOL removeSuccess = [token remove];
+                NSAssert(removeSuccess, @"AspectToken remove not success");
+            }
+        }
+        id newDelegate = change[NSKeyValueChangeNewKey];
+        if (newDelegate) {
+            id<AspectToken> token = objc_getAssociatedObject(newDelegate, @selector(yndwb_hookLoadingForUIIfNeed:));
+            NSAssert(token == nil, @"AspectToken is not nil");
+            token = [newDelegate aspect_hookSelector:@selector(webViewDidFinishLoad:) withOptions:AspectPositionAfter usingBlock:^(id<AspectInfo> aspectInfo) {
+                __strong typeof(self) self = wself;
+                
+            } error:NULL];
+        }
+    }];
+    return YES;
 }
 
 #pragma mark - detect
