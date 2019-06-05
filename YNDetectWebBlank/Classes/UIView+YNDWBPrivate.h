9f3a81692245a2cac2c2466be5e74007f226d0e4
Yanni <wy19900729@gmail.com>
2019-06-05 20:54:01 +0800

    no message

--- YNDetectWebBlank/Classes/UIView+YNDWBPrivate.h
+++ YNDetectWebBlank/Classes/UIView+YNDWBPrivate.h
@@ -11,7 +11,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-typedef void (^YNDWBDidMoveToWindowBlock)(UIWindow *window);
+typedef void (^YNDWBDidMoveToWindowBlock)(UIWindow * _Nullable window);
 
 @interface UIView (YNDWBPrivate)
 
